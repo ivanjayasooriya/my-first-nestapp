cd464b043e81e0ed94fa0d4916286b9ff0fc4819
Ivan Adithya <ivanjayasooriya03@gmail.com>
2026-09-04 16:35:03 +0530

    feat: refactor AuthModule to include Passport and JWT strategies, and update JWT configuration

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,15 +2,24 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service.js';
 import { AuthController } from './auth.controller.js';
 import { JwtModule } from '@nestjs/jwt';
+import { UserModule } from '../user/user.module.js';
+import { PassportModule } from '@nestjs/passport';
+import { LocalStrategy } from './local.strategy.js';
+import { JwtStrategy } from './jwt.strategy.js';
 
 @Module({
   imports: [
-    JwtModule.register({
-      secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: Number(process.env.JWT_EXPIRES_IN) },
-    }),
+    UserModule,
+    PassportModule,
+    JwtModule.registerAsync({
+      useFactory: () => ({
+        secret: 'your-secret-key',
+        signOptions: { expiresIn: '1h' },
+      }),
+    })
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
+  exports: [AuthService]
 })
 export class AuthModule {}
